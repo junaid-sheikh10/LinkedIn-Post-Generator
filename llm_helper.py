0fc23388f6d912a9e0a517c29cba3b83edec3822
junaid-sheikh10 <sheikhjunaid804@gmail.com>
2025-09-25 12:46:12 +0200

    preprocess update

--- llm_helper.py
+++ llm_helper.py
@@ -6,5 +6,8 @@ load_dotenv()
 groq_api_key=os.getenv("GROK_API")
 
 llm=ChatGroq(api_key=groq_api_key,model="llama-3.3-70b-versatile")
-llm_response=llm.invoke("What are the main ingredients of a Margherita pizza?")
-print(llm_response.content)
+#llm_response=llm.invoke("What are the main ingredients of a Margherita pizza?")
+
+if __name__=="__main__":
+    response = llm.invoke("Two most important thing is samosa are")
+    print(response.content)
